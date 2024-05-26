1485872a71e6808b57f1fdf330214bb6b9898de0
bhupeshlahare <bhupeshlahare@gmail.com>
2024-05-27 00:43:27 +0530

    Read pdf and create chunks

---
+++ chatbot.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+#upload pdf files
+st.header("My first chatbot")
+
+with st.sidebar:
+    st.title("Your Documents")
+    file = st.file_uploader("Upload a PDF file and start asking questions", type="pdf")
+
+#Extract the text
+if file is not None:
+    pdf_reader = PdfReader(file)
+    text = ""
+    for pages in pdf_reader.pages:
+        text+= pages.extract_text()
+        #st.write(text)
+
+#Break it into chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators="\n",
+        chunk_size=1000,
+        chunk_overlap=150,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    st.write(chunks)
