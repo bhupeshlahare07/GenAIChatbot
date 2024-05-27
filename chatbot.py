d692b056ff36d7fd3ca1d537f1a8357a22e8674c
bhupeshlahare <bhupeshlahare@gmail.com>
2024-05-27 22:33:54 +0530

    Define LLM

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,10 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain_community.chat_models import ChatOpenAI
 
+#openAI key
 
 
 #upload pdf files
@@ -29,10 +32,32 @@ if file is not None:
         length_function=len
     )
     chunks = text_splitter.split_text(text)
-    st.write(chunks)
+    #st.write(chunks)
 
     # generating embedding
     embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
     # creating vector store - FAISS
     vector_store = FAISS.from_texts(chunks, embeddings)
+
+    #get user questions
+    user_question = st.text_input("Type your question here")
+
+    # do similarity search
+    if user_question:
+        match = vector_store.similarity_search(user_question)
+        #st.write(match)
+
+        # define the LLM
+        llm = ChatOpenAI(
+            openai_api_key=OPENAI_API_KEY,
+            temperature=0,
+            max_tokens=1000,
+            model_name="gpt-3.5-turbo"
+        )
+
+        # output results
+        # chain -> take the question, get relevant document, pass it to the LLM, generate the output
+        chain = load_qa_chain(llm, chain_type="stuff")
+        response = chain.run(input_documents=match, question=user_question)
+        st.write(response)
